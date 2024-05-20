4bd80104e0730ea43658edfd834c547c489a85c4
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-05-20 17:46:24 +0100

    Database connection and Env vars

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"goncalojrmosa/jwtapi/cmd/api"
+	"goncalojrmosa/jwtapi/config"
+	"goncalojrmosa/jwtapi/db"
 	"log"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr: config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
 	sv := api.NewAPIServer(":8080", nil)
 	if err := sv.Run(); err != nil{
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB)  {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database connection established")
 }
