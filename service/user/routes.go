cb8d1273a26f7962b718d8ac4f5355c06becd644
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-05-21 16:45:52 +0100

    user registration

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,16 +1,21 @@
 package user
 
 import (
+	"goncalojrmosa/jwtapi/types"
+	"goncalojrmosa/jwtapi/utils"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{
+		store: store,
+	}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -24,5 +29,15 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	// ...
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	// check if user exists
+
+
+
+	// if not, create user
 }
